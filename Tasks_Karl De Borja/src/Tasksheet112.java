85fb1c8f984b9e6a59aeffd38a617485ea421092
karldeborjacom <145963912+karldeborjacom@users.noreply.github.com>
2024-12-07 14:51:29 +0800

    Add files via upload

---
+++ Tasks_Karl De Borja/src/Tasksheet112.java
@@ -0,0 +1,19 @@
+public class Tasksheet112 {
+    public static void main(String[] args) {
+
+        char h = 'H';
+        int three = 3;
+        int one = 1;
+        int zero = 0;
+        char w = 'w';
+        char r = 'r';
+        char d = 'd';
+        float twoPointZero = 2.0f;
+        boolean isTrue = true;
+
+        String output = "" + h + three + one + one + zero + " " + w + 0 + r + one + d + " " + twoPointZero + " " + isTrue;
+
+
+        System.out.println(output);
+    }
+}
